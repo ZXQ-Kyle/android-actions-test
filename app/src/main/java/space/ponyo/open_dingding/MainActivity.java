6f102860b5ee6a4eae59c032758e0a5e580d5cc4
zxq <674079013@qq.com>
2023-07-28 17:43:29 +0800

    完成功能

--- app/src/main/java/space/ponyo/open_dingding/MainActivity.java
+++ app/src/main/java/space/ponyo/open_dingding/MainActivity.java
@@ -1,39 +1,54 @@
 package space.ponyo.open_dingding;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import java.net.URLEncoder;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(view -> startDingtalkApp(this));
     }
 
     @Override
     protected void onResume() {
-        startDingtalkApp(this, "");
-        super.onResume()
+        Timer timer = new Timer();
+        Context _this = this;
+        int delay = (int) (Math.random() * 3);
+        btn.setText(String.valueOf(delay));
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startDingtalkApp(_this);
+            }
+        }, delay * 60 * 1000L);
+
+        super.onResume();
     }
 
     /**
      * 打开钉钉客户端 并在钉钉客户端打开传入的指定url
      *
      * @param context 安卓上下文环境，推荐Activity Context
-     * @param url     需要在钉钉客户端打开的页面地址
      */
-    private void startDingtalkApp(Context context, String url) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-
-        String jumpUrl = "dingtalk://dingtalkclient/page/link?url=" + URLEncoder.encode(url); //必须对url做encode
-        Uri uri = Uri.parse(jumpUrl);
-        intent.setData(uri);
-        if (null != intent.resolveActivity(context.getPackageManager())) {
-            context.startActivity(intent);
+    private void startDingtalkApp(Context context) {
+        Intent intent = getPackageManager().getLaunchIntentForPackage("com.alibaba.android.rimet");
+        if (intent != null) {
+            intent.putExtra("type", "110");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
         }
     }
 }
