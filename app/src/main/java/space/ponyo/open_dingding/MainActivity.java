072e19982735a283fe55a70085ac80f0058116c0
zxq <674079013@qq.com>
2023-10-11 11:32:10 +0800

    完成电量消息发送

--- app/src/main/java/space/ponyo/open_dingding/MainActivity.java
+++ app/src/main/java/space/ponyo/open_dingding/MainActivity.java
@@ -1,14 +1,17 @@
 package space.ponyo.open_dingding;
 
-import android.content.Context;
-import android.content.Intent;
+import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,35 +22,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = (Button) findViewById(R.id.btn);
-        btn.setOnClickListener(view -> startDingtalkApp(this));
     }
 
     @Override
     protected void onResume() {
-        Timer timer = new Timer();
-        Context _this = this;
-        int delay = (int) (Math.random() * 10);
-        btn.setText(String.valueOf(delay) + " 分钟");
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                startDingtalkApp(_this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            try {
+                notifyElectricity();
+            } catch (MalformedURLException e) {
+                btn.setText(e.toString());
             }
-        }, delay * 60 * 1000L);
+        }
         super.onResume();
     }
 
-    /**
-     * 打开钉钉客户端 并在钉钉客户端打开传入的指定url
-     *
-     * @param context 安卓上下文环境，推荐Activity Context
-     */
-    private void startDingtalkApp(Context context) {
-        Intent intent = getPackageManager().getLaunchIntentForPackage("com.alibaba.android.rimet");
-        if (intent != null) {
-            intent.putExtra("type", "110");
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void notifyElectricity() throws MalformedURLException {
+        BatteryManager manager = (BatteryManager) getSystemService(BATTERY_SERVICE);
+        int capacity = manager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+//        UNKNOWN=1，CHARGING=2，DISCHARGING=3，NOT_CHARGING=4，FULL=5
+        int statusInt = manager.getIntProperty(BatteryManager.BATTERY_PROPERTY_STATUS);///充电状态
+
+        String status = "";
+        switch (statusInt) {
+            case 2: {
+                status = "充电中";
+                break;
+            }
+            case 3:
+            case 4: {
+                status = "未充电";
+                break;
+            }
+            case 5: {
+                status = "已充满";
+                break;
+            }
+            default: {
+                status = "未知状态";
+                break;
+            }
         }
+
+        URL url = new URL("http://push.ponyo.space/wecomchan?sendkey=ponyo&&msg_type=text&&msg=充电状态：" + status + "，当前电量：" + capacity);
+        new Thread(() -> {
+            try {
+                // 1.打开 URLConnection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.getResponseCode();
+                runOnUiThread(() -> btn.setText("发送成功"));
+            } catch (IOException e) {
+                runOnUiThread(() -> btn.setText(e.toString()));
+            }
+        }).start();
     }
 }
